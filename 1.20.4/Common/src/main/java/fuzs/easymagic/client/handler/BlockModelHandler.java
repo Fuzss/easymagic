f98eacb3a82773fd556c06422aa476ed3409b26e
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-01-23 23:30:45 +0100

    Update BlockModelHandler.java

--- 1.20.4/Common/src/main/java/fuzs/easymagic/client/handler/BlockModelHandler.java
+++ 1.20.4/Common/src/main/java/fuzs/easymagic/client/handler/BlockModelHandler.java
@@ -1,14 +1,20 @@
 package fuzs.easymagic.client.handler;
 
+import com.google.common.base.Suppliers;
+import com.google.common.collect.Maps;
 import fuzs.easymagic.init.ModRegistry;
 import fuzs.puzzleslib.api.event.v1.core.EventResultHolder;
 import net.minecraft.Util;
 import net.minecraft.client.renderer.block.BlockModelShaper;
+import net.minecraft.client.resources.model.ModelResourceLocation;
 import net.minecraft.client.resources.model.UnbakedModel;
-import net.minecraft.core.registries.BuiltInRegistries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.block.state.StateDefinition;
+import net.minecraft.world.level.block.state.properties.Property;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.Map;
 import java.util.function.BiConsumer;
@@ -16,22 +22,46 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class BlockModelHandler {
-    private static final Map<ResourceLocation, ResourceLocation> BLOCK_CONVERSIONS;
+    private static final Supplier<Map<ResourceLocation, ResourceLocation>> MODEL_LOCATIONS;
 
     static {
-        BLOCK_CONVERSIONS = Map.of(ModRegistry.ENCHANTMENT_TABLE_BLOCK.value(), Blocks.ENCHANTING_TABLE).entrySet().stream().map(BlockModelHandler::getAnyBlockStateConversion).collect(Util.toMap());
+        MODEL_LOCATIONS = Suppliers.memoize(() -> {
+            return Map.of(ModRegistry.ENCHANTMENT_TABLE_BLOCK.value(), Blocks.ENCHANTING_TABLE).entrySet().stream().flatMap(entry -> {
+                return convertAllBlockStates(entry.getKey(), entry.getValue()).entrySet().stream();
+            }).collect(Util.toMap());
+        });
     }
 
     public static EventResultHolder<UnbakedModel> onModifyUnbakedModel(ResourceLocation modelLocation, Supplier<UnbakedModel> unbakedModel, Function<ResourceLocation, UnbakedModel> modelGetter, BiConsumer<ResourceLocation, UnbakedModel> modelAdder) {
-        modelLocation = new ResourceLocation(modelLocation.getNamespace(), modelLocation.getPath());
-        if (BLOCK_CONVERSIONS.containsKey(modelLocation)) {
-            return EventResultHolder.interrupt(modelGetter.apply(BLOCK_CONVERSIONS.get(modelLocation)));
+        if (MODEL_LOCATIONS.get().containsKey(modelLocation)) {
+            return EventResultHolder.interrupt(modelGetter.apply(MODEL_LOCATIONS.get().get(modelLocation)));
         } else {
             return EventResultHolder.pass();
         }
     }
 
-    private static Map.Entry<ResourceLocation, ResourceLocation> getAnyBlockStateConversion(Map.Entry<Block, Block> blocks) {
-        return Map.entry(BuiltInRegistries.BLOCK.getKey(blocks.getKey()), BlockModelShaper.stateToModelLocation(blocks.getValue().defaultBlockState()));
+    private static Map<ModelResourceLocation, ModelResourceLocation> convertAllBlockStates(Block oldBlock, Block newBlock) {
+        Map<ModelResourceLocation, ModelResourceLocation> modelLocations = Maps.newHashMap();
+        for (BlockState oldBlockState : oldBlock.getStateDefinition().getPossibleStates()) {
+            BlockState newBlockState = convertBlockState(newBlock.getStateDefinition(), oldBlockState);
+            modelLocations.put(BlockModelShaper.stateToModelLocation(oldBlockState), BlockModelShaper.stateToModelLocation(newBlockState));
+        }
+        return modelLocations;
+    }
+
+    private static BlockState convertBlockState(StateDefinition<Block, BlockState> newStateDefinition, BlockState oldBlockState) {
+        BlockState newBlockState = newStateDefinition.any();
+        for (Map.Entry<Property<?>, Comparable<?>> entry : oldBlockState.getValues().entrySet()) {
+            newBlockState = setBlockStateValue(entry.getKey(), entry.getValue(), newStateDefinition::getProperty, newBlockState);
+        }
+        return newBlockState;
+    }
+
+    private static <T extends Comparable<T>, V extends T> BlockState setBlockStateValue(Property<?> oldProperty, Comparable<?> oldValue, Function<String, @Nullable Property<?>> propertyGetter, BlockState blockState) {
+        Property<?> newProperty = propertyGetter.apply(oldProperty.getName());
+        if (newProperty != null) {
+            return blockState.setValue((Property<T>) newProperty, (V) oldValue);
+        }
+        return blockState;
     }
 }
