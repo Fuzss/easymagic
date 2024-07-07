4d7ff85ffa87a1cde4a0345192b6f7b9c3df7bc8
Fuzss <28218241+Fuzss@users.noreply.github.com>
2024-07-07 08:54:17 +0200

    update puzzles lib

--- 1.21/Common/src/main/java/fuzs/easymagic/world/level/block/entity/EnchantmentTableWithInventoryBlockEntity.java
+++ 1.21/Common/src/main/java/fuzs/easymagic/world/level/block/entity/EnchantmentTableWithInventoryBlockEntity.java
@@ -5,6 +5,7 @@ import fuzs.easymagic.config.ServerConfig;
 import fuzs.easymagic.init.ModRegistry;
 import fuzs.easymagic.world.inventory.ModEnchantmentMenu;
 import fuzs.puzzleslib.api.block.v1.entity.TickingBlockEntity;
+import fuzs.puzzleslib.api.container.v1.ListBackedContainer;
 import net.minecraft.core.BlockPos;
 import net.minecraft.core.Direction;
 import net.minecraft.core.HolderLookup;
@@ -12,7 +13,10 @@ import net.minecraft.core.NonNullList;
 import net.minecraft.nbt.CompoundTag;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.protocol.game.ClientboundBlockEntityDataPacket;
-import net.minecraft.world.*;
+import net.minecraft.world.ContainerHelper;
+import net.minecraft.world.LockCode;
+import net.minecraft.world.MenuProvider;
+import net.minecraft.world.WorldlyContainer;
 import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.inventory.AbstractContainerMenu;
@@ -25,10 +29,8 @@ import net.minecraft.world.level.block.entity.EnchantingTableBlockEntity;
 import net.minecraft.world.level.block.state.BlockState;
 import org.jetbrains.annotations.Nullable;
 
-public class EnchantmentTableWithInventoryBlockEntity extends EnchantingTableBlockEntity implements Container, MenuProvider, WorldlyContainer, TickingBlockEntity {
+public class EnchantmentTableWithInventoryBlockEntity extends EnchantingTableBlockEntity implements ListBackedContainer, MenuProvider, WorldlyContainer, TickingBlockEntity {
     private final NonNullList<ItemStack> items = NonNullList.withSize(3, ItemStack.EMPTY);
-    private final int[] itemSlots = new int[]{0};
-    private final int[] catalystSlots = EasyMagic.CONFIG.get(ServerConfig.class).dedicatedRerollCatalyst ? new int[]{1, 2} : new int[]{1};
     private LockCode code = LockCode.NO_LOCK;
 
     public EnchantmentTableWithInventoryBlockEntity(BlockPos blockPos, BlockState blockState) {
@@ -75,41 +77,13 @@ public class EnchantmentTableWithInventoryBlockEntity extends EnchantingTableBlo
     }
 
     @Override
+    public NonNullList<ItemStack> getContainerItems() {
+        return this.items;
+    }
+
+    @Override
     public int getContainerSize() {
         return EasyMagic.CONFIG.get(ServerConfig.class).dedicatedRerollCatalyst ? 3 : 2;
-    }
-
-    @Override
-    public boolean isEmpty() {
-        for (ItemStack itemstack : this.items) {
-            if (!itemstack.isEmpty()) {
-                return false;
-            }
-        }
-        return true;
-    }
-
-    @Override
-    public ItemStack getItem(int index) {
-        return index >= 0 && index < this.getContainerSize() ? this.items.get(index) : ItemStack.EMPTY;
-    }
-
-    @Override
-    public ItemStack removeItem(int index, int count) {
-        return ContainerHelper.removeItem(this.items, index, count);
-    }
-
-    @Override
-    public ItemStack removeItemNoUpdate(int index) {
-        return ContainerHelper.takeItem(this.items, index);
-    }
-
-    @Override
-    public void setItem(int index, ItemStack stack) {
-        if (index >= 0 && index < this.getContainerSize()) {
-            this.items.set(index, stack);
-            this.setChanged();
-        }
     }
 
     @Override
@@ -122,17 +96,11 @@ public class EnchantmentTableWithInventoryBlockEntity extends EnchantingTableBlo
     }
 
     @Override
-    public void clearContent() {
-        this.items.clear();
-        this.setChanged();
-    }
-
-    @Override
-    public boolean canPlaceItem(int index, ItemStack stack) {
+    public boolean canPlaceItem(int index, ItemStack itemStack) {
         if (index == 2) {
-            return stack.is(ModRegistry.REROLL_CATALYSTS_ITEM_TAG);
+            return itemStack.is(ModRegistry.REROLL_CATALYSTS_ITEM_TAG);
         } else if (index == 1) {
-            return stack.is(ModRegistry.ENCHANTING_CATALYSTS_ITEM_TAG);
+            return itemStack.is(ModRegistry.ENCHANTING_CATALYSTS_ITEM_TAG);
         } else if (index == 0) {
             return this.items.get(0).isEmpty();
         }
@@ -140,19 +108,23 @@ public class EnchantmentTableWithInventoryBlockEntity extends EnchantingTableBlo
     }
 
     @Override
-    public int[] getSlotsForFace(Direction side) {
-        return side.getAxis().isHorizontal() ? this.catalystSlots : this.itemSlots;
+    public int[] getSlotsForFace(Direction direction) {
+        if (direction.getAxis().isHorizontal()) {
+            return EasyMagic.CONFIG.get(ServerConfig.class).dedicatedRerollCatalyst ? new int[]{1, 2} : new int[]{1};
+        } else {
+            return new int[]{0};
+        }
     }
 
     @Override
-    public boolean canPlaceItemThroughFace(int index, ItemStack itemStackIn, @Nullable Direction direction) {
-        return this.canPlaceItem(index, itemStackIn);
+    public boolean canPlaceItemThroughFace(int index, ItemStack itemStack, @Nullable Direction direction) {
+        return this.canPlaceItem(index, itemStack);
     }
 
     @Override
-    public boolean canTakeItemThroughFace(int index, ItemStack stack, Direction direction) {
+    public boolean canTakeItemThroughFace(int index, ItemStack itemStack, Direction direction) {
         // only allow extracting of enchantable item
-        return index == 0 && (stack.isEnchanted() || stack.getItem() instanceof EnchantedBookItem);
+        return index == 0 && (itemStack.isEnchanted() || itemStack.getItem() instanceof EnchantedBookItem);
     }
 
     @Override
